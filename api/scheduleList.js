b1415305956dd9811a081b582bdd1dbafa18d9bc
Tester Inet Doc <tester.inet.doc@gmail.com>
2026-07-16 11:05:18 +0700

    test

--- api/scheduleList.js
+++ api/scheduleList.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { cookie, timeout } from './env.js';
 
 export function scheduleList() {
-    const url = 'https://uat-dtamnext.one.th/api/v1/slot/schedule-list?date=2026-07-10&service_id=&service_point_id=&uid=&start_time=&end_time=&page=1&limit=20';
+    const url = 'https://uat-dtamnext.one.th/api/v1/slot/schedule-list?date=2026-07-16&service_id=&service_point_id=&uid=&start_time=&end_time=&page=1&limit=20';
 
     const headers = {
         Cookie: '' + cookie,
