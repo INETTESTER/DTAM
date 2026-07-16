7010b9f097e85bd6dae1dc2e70725fd485aad8ad
Tester Inet Doc <tester.inet.doc@gmail.com>
2026-07-16 11:13:32 +0700

    test

--- api/timeslotsByDate.js
+++ api/timeslotsByDate.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { cookie, timeout } from './env.js';
 
 export function timeslotsByDate() {
-    const url = 'https://uat-dtamnext.one.th/api/v1/slot/timeslots-by-date?date=2026-07-10&service_id=&service_point_id=&uid=';
+    const url = 'https://uat-dtamnext.one.th/api/v1/slot/timeslots-by-date?date=2026-07-16&service_id=&service_point_id=&uid=';
 
     const headers = {
         Cookie: '' + cookie,
