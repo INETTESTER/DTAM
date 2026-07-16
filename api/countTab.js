5737a89240c3732975c37bf2995552334e4e01e1
Tester Inet Doc <tester.inet.doc@gmail.com>
2026-07-16 12:31:56 +0700

    test

--- api/countTab.js
+++ api/countTab.js
@@ -5,7 +5,7 @@ export function countTab() {
     const url = 'https://uat-dtamnext.one.th/api/v1/visits/count-tab?pid=&service_id=&service_point_id=&date=';
 
     const headers = {
-        Cookie: '' + cookie,
+        Cookie: 'session=1d45ce39-1b2e-4ff2-86d2-066909cd88d4',
     };
 
     const response = http.get(url, {
--- api/getVisitList.js
+++ api/getVisitList.js
@@ -5,7 +5,7 @@ export function getVisitList() {
     const url = 'https://uat-dtamnext.one.th/api/v1/visits/get-visit-list?pid=&page=1&limit=10&status=%E0%B8%97%E0%B8%B1%E0%B9%89%E0%B8%87%E0%B8%AB%E0%B8%A1%E0%B8%94&service_id=&service_point_id=&order_by=DESC&date=';
 
     const headers = {
-        Cookie: '' + cookie,
+        Cookie: 'session=1d45ce39-1b2e-4ff2-86d2-066909cd88d4',
     };
 
     const response = http.get(url, {
