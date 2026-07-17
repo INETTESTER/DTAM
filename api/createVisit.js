651d12bd1b8ae2676cc4a1943959616cca4cf2c2
Tester Inet Doc <tester.inet.doc@gmail.com>
2026-07-17 15:43:26 +0700

    test

--- api/createVisit.js
+++ api/createVisit.js
@@ -5,18 +5,18 @@ export function createVisit() {
     const url = 'https://uat-dtamnext.one.th/api/v1/visits/create-visit';
 
     const payload = JSON.stringify({
-        uid: "27",
-        pid: "45",
-        visit_date: "2026-07-16",
-        visit_start_time: "18:30:00",
-        visit_end_time: "19:00:00",
-        service_type: "online",
-        email: "test@gmail.com",
-        phone: null,
-        note: null,
-        type_right: "SSS",
-        type_right_text: "สิทธิประกันสังคม",
-        is_self_pay: 0,
+        "uid": "39",
+        "pid": "306280",
+        "visit_date": "2026-07-17",
+        "visit_start_time": "16:00:00",
+        "visit_end_time": "16:15:00",
+        "service_type": "online",
+        "email": "",
+        "phone": null,
+        "note": null,
+        "type_right": "SSS",
+        "type_right_text": "สิทธิประกันสังคม",
+        "is_self_pay": 0
     });
 
     const headers = {
@@ -29,7 +29,7 @@ export function createVisit() {
         timeout: timeout,
     });
 
-    //console.log(`Response body: ${response.body}`);
+    // console.log(`Response body: ${response.body}`);
 
     return response;
 }
